ff18c4a73510d3683b5322ba4a05c7d25409b331
Manuel Leduc <manuel.leduc@inria.fr>
2015-08-18 14:35:53 +0200

    First commit

---
+++ LocalChannel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Org.Kevoree.Annotation;
+using Org.Kevoree.Core.Api;
+
+namespace kevoreedotnetchannellocal
+{
+	[ChannelType]
+	public class LocalChannel:ChannelDispatch
+	{
+		[KevoreeInject]
+		private ChannelContext channelContext;
+
+		public LocalChannel ()
+		{
+		}
+
+		#region ChannelDispatch implementation
+
+		void ChannelDispatch.dispatch (string payload, Callback callback)
+		{
+			foreach (var p in channelContext.getLocalPorts()) {
+				p.send (payload, callback);
+			}
+		}
+
+		#endregion
+	}
+}
