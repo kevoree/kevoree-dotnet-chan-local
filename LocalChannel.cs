90ca2086482009e6307fb92aba18697dbe5edce8
Manuel Leduc <manuel.leduc@inria.fr>
2015-10-25 14:08:00 +0100

    BindChannel integration

--- LocalChannel.cs
+++ LocalChannel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Org.Kevoree.Annotation;
 using Org.Kevoree.Core.Api;
 
@@ -8,8 +9,7 @@ namespace Org.Kevoree.Library
     [Serializable]
     public class LocalChannel : MarshalByRefObject, DeployUnit, ChannelDispatch
 	{
-		[KevoreeInject]
-		private ChannelContext channelContext;
+	    private List<Port> _inputPorts = new List<Port>();
 
 		public LocalChannel ()
 		{
@@ -19,11 +19,22 @@ namespace Org.Kevoree.Library
 
 		void ChannelDispatch.dispatch (string payload, Callback callback)
 		{
-			foreach (var p in channelContext.getLocalPorts()) {
+			foreach (var p in _inputPorts) {
 				p.send (payload, callback);
 			}
+
 		}
 
-		#endregion
+	    public void registerPort(Port p)
+	    {
+	        this._inputPorts.Add(p);
+	    }
+
+	    public void removePort(Port p)
+	    {
+	        this._inputPorts.Remove(p);
+	    }
+
+	    #endregion
 	}
 }
